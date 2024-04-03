f3e7f9940d3cf86d0fe6fbf5f06a36352c988050
Renan <renanrapace13@gmail.com>
2024-04-03 19:33:44 -0300

    Third commit

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,11 @@
 import fastify from "fastify";
 import cookie from "@fastify/cookie";
+import WebSocket from "@fastify/websocket";
 
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
 import { voteOnPoll } from "./routes/vote-on-poll";
+import { pollResults } from "./ws/poll-result";
 
 const app = fastify();
 
@@ -12,9 +14,13 @@ app.register(cookie, {
   hook: "onRequest", // set to false to disable cookie autoparsing or set autoparsing on any of the following hooks: 'onRequest', 'preParsing', 'preHandler', 'preValidation'. default: 'onRequest'
 });
 
+app.register(WebSocket);
+
 app.register(createPoll);
 app.register(getPoll);
 app.register(voteOnPoll);
+
+app.register(pollResults);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP Server Running");
---
+++ src/http/ws/poll-result.ts
@@ -0,0 +1,23 @@
+import z from "zod";
+
+import { FastifyInstance } from "fastify";
+
+import { voting } from "../../utils/voting-pub-sub";
+
+export async function pollResults(app: FastifyInstance) {
+  app.get(
+    "/polls/:pollId/results",
+    { websocket: true },
+    (connection, request) => {
+      const getPollParams = z.object({
+        pollId: z.string().uuid(),
+      });
+
+      const { pollId } = getPollParams.parse(request.params);
+
+      voting.subscribe(pollId, (message) => {
+        connection.send(JSON.stringify(message));
+      });
+    }
+  );
+}
